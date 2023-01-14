9b6eabfef658e0fd771fc3a4f503d4392a83eb38
Fatma Oymak <bedizma@hotmail.com>
2023-01-14 00:22:01 +0000

    answer is

---
+++ script.js
@@ -0,0 +1,11 @@
+songsToPlay = [
+    "Grace Kelly",
+    "I want it That Way",
+    "Come to Eileen",
+    "Groove Is in the Heart",
+    "Careless Whisper",
+];
+
+console.log(songsToPlay.length);
+console.log(songsToPlay.reverse());
+console.log(songsToPlay.sort());
